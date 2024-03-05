defdaeb5317cb395a921d87990c11a33445b28f5
z-shaffer <zshaffer@protonmail.com>
2024-03-05 17:20:48 +0100

    Implemented public key and signatures

--- crypto/keys.go
+++ crypto/keys.go
@@ -33,8 +33,10 @@ func (p *PrivateKey) Bytes() []byte {
 	return p.key
 }
 
-func (p *PrivateKey) Sign(msg []byte) []byte {
-	return ed25519.Sign(p.key, msg)
+func (p *PrivateKey) Sign(msg []byte) *Signature {
+	return &Signature{
+		value: ed25519.Sign(p.key, msg),
+	}
 }
 
 func (p *PrivateKey) Public() *PublicKey {
@@ -48,4 +50,19 @@ func (p *PrivateKey) Public() *PublicKey {
 
 type PublicKey struct {
 	key ed25519.PublicKey
+}
+
+func (p *PublicKey) Bytes() []byte {
+	return p.key
+}
+
+type Signature struct {
+	value []byte
+}
+
+func (s *Signature) Bytes() []byte {
+	return s.value
+}
+func (s *Signature) Verify(pubKey *PublicKey, msg []byte) bool {
+	return ed25519.Verify(pubKey.key, msg, s.value)
 }
--- crypto/keys_test.go
+++ crypto/keys_test.go
@@ -7,6 +7,17 @@ import (
 
 func TestGeneratePrivateKey(t *testing.T) {
 	privKey := GeneratePrivateKey()
-
 	assert.Equal(t, len(privKey.Bytes()), privKeyLen)
+
+	pubKey := privKey.Public()
+	assert.Equal(t, len(pubKey.Bytes()), pubKeyLen)
+}
+func TestPrivateKeySign(t *testing.T) {
+	privKey := GeneratePrivateKey()
+	pubKey := privKey.Public()
+	msg := []byte("Testing Private Key Sign")
+	sig := privKey.Sign(msg)
+	
+	assert.True(t, sig.Verify(pubKey, msg))
+	assert.False(t, sig.Verify(pubKey, []byte("test")))
 }
